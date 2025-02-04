b41eb30b948b9584653bacb83a35acdcf5a6ff37
Nigel <barrowjr0@gmail.com>
2025-02-04 11:09:05 -0500

    Add Clap

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,13 @@
+use clap::Parser;
+/// Flattens a folder structure
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Delete original files after flattening
+    #[arg(short, long)]
+    delete: String,
+}
+
 fn main() {
+    let args = Args::parse();
 }
